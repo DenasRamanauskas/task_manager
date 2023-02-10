5755b9214757b051942382a55140d2894232447f
DenasRamanauskas <denas.ramanauskas@gmail.com>
2023-02-10 23:56:04 +0200

    Initial commit

---
+++ mysite/todolist/forms.py
@@ -0,0 +1,13 @@
+
+from .models import Task
+from django import forms
+from django.contrib.auth.models import User
+
+class DateTimeInput(forms.DateTimeInput):
+    input_type = "datetime-local"
+
+class UserUzsakymasCreateForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['text', 'tbd']
+        widgets = {'tbd': DateTimeInput()}
